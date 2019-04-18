7fc9d4e2af1062fe0776bbea509e732edb80a781
gabriel benatar <gabriel.benatar@beityaacov.com.br>
2019-04-18 17:24:40 -0300

    Zerei e recomeçei

---
+++ readme.py
@@ -0,0 +1,11 @@
+Exercício Programa - 2019/1 - Design de Software
+------------------------------------------------
+
+Alunos: 
+- Matheus Freitas Sant'Ana, matheusfs2@al.insper.edu.br
+- Gabriel Salvator Benatar, gabriel.benatar@beityaacov.com.br
+
+
+
+
+
