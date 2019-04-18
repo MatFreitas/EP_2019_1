cc97fc79987a8634cf19311eae8d42b1736ec28a
gabriel benatar <gabriel.benatar@beityaacov.com.br>
2019-04-18 17:42:57 -0300

    Introdução

--- ep1.py
+++ ep1.py
@@ -3,5 +3,9 @@
 # Alunos: 
 #- Matheus Freitas Sant'Ana, matheusfs2@al.insper.edu.br
 #- Gabriel Salvator Benatar, gabriel.benatar@beityaacov.com.br
-
-
+print()
+print("Sala 405- Aula de Design de Software")
+print()
+print('-'*37)
+print()
+print('É Quarta-Feira a tarde, você está no Insper na Aula de Design de Software e decide ir ao banheiro, pois precisa de um lugar sossegado para pensar em uma maneira de como adiar o EP, já que você nem começou a fazer. De repente seu avô Rick que estava desaparecido há anos surge e te convida para uma aventura diferenciada, sobre realidades relativas, mudar o futuro etc. Você não entende muito bem o que ele quer dizer com isso, mas vê isso como uma possibilidade de adiar o EP.')
