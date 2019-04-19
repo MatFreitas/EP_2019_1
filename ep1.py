c684a40f41b58385ddd85303dabaa15b61979e96
MatFreitas <matheusfs2@al.insper.edu.br>
2019-04-19 11:59:13 -0300

    Correção erros de português e de espaços

--- ep1.py
+++ ep1.py
@@ -5,10 +5,8 @@
 #- Gabriel Salvator Benatar, gabriel.benatar@beityaacov.com.br
 
 usuario=input("Para começar a jogar digite seu nome: ")
-print('-'*37)
+print()
 print()
 print("Sala 405- Aula de Design de Software")
-print()
 print('-'*37)
-print()
-print('É Quarta-Feira a tarde, você está no Insper na Aula de Design de Software e decide ir ao banheiro, pois precisa de um lugar sossegado para pensar em uma maneira de como adiar o EP, já que você nem começou a fazer. De repente seu avô Rick que estava desaparecido há anos surge e te convida para uma aventura diferenciada, sobre realidades relativas, mudar o futuro etc. Você não entende muito bem o que ele quer dizer com isso, mas vê isso como uma possibilidade de adiar o EP.')
+print('É Quarta-Feira à tarde, você está no Insper na Aula de Design de Software e decide ir ao banheiro, pois precisa de um lugar sossegado para pensar em uma maneira de como adiar o EP, já que você nem começou a fazer. De repente seu avô Rick que estava desaparecido há anos surge e te convida para uma aventura diferenciada, sobre realidades alternativas, mudar o futuro, etc. Você não entende muito bem o que ele quer dizer, mas vê isso como uma possibilidade de adiar o EP.')
