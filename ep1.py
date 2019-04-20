5580a36ed10ff8b73052ccff0b07bc0aa50a9784
MatFreitas <matheusfs2@al.insper.edu.br>
2019-04-20 11:05:27 -0300

    Adicionei sistema de validação de respostas

--- ep1.py
+++ ep1.py
@@ -4,6 +4,7 @@
 #- Matheus Freitas Sant'Ana, matheusfs2@al.insper.edu.br
 #- Gabriel Salvator Benatar, gabriel.benatar@beityaacov.com.br
 inventario={}
+vida=200
 usuario=input("Para começar a jogar digite seu nome: ")
 print()
 print()
@@ -14,6 +15,10 @@ print()
 print("Rick: Rápido! Preciso de sua ajuda, a Teriathlon Plasmium 26 quebrou! Se você me ajudar, te retribuo com um favor!")
 print()
 escolha1=input("aceitar ou recusar? ")
+if escolha1!="aceitar" and escolha1!="recusar":
+    while escolha1!="aceitar" and escolha1!="recusar":
+        print()
+        escolha1=input("Rick: 'Fala que nem gente!' (digite uma resposta válida)")       
 if escolha1=="aceitar":
     print()
     print("Rick começa a te explicar que precisa consertar sua arma. Para isso, é necessário que vocês visitem 3 realidades diferentes para conseguir os componentes que constituem sua arma: o 'Bowl', uma pedra de urânio e um frasco de magnésio.")
@@ -21,10 +26,15 @@ else:
     print()
     print("Rick insiste que você deve ajudá-lo  dizendo que se for com ele, além de te ajudar com a data, também te ajudará a tirar um 10 no EP.")
     escolha1=input("aceitar ou recusar? ")
+    if escolha1!="aceitar" and escolha1!="recusar":
+        while escolha1!="aceitar" and escolha1!="recusar":
+            print()
+            escolha1=input("Rick: 'Fala que nem gente!' (digite uma resposta válida)") 
     if escolha1=="aceitar":
         print()
         print("Que a aventura começe!")
     else:
         print("{0} é teletransportado para a garagem de seu avô e uma arma intergaláctica é adicionada ao seu inventário para ser usada no futuro.".format(usuario))
         inventario["arma intergaláctica"]=50
+
         
